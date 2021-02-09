fcd9198168013b8c71f192a19448dd48e14627e7
Balachanderrao <palepubalachanderrao25@gmail.com>
2021-02-09 19:21:22 +0530

    added comments and functionality added to contact form

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,7 @@
+/* App.js
+Balachander Rao Palepu, 301170247, 07-02-2021
+*/
+
 (function () {
   function Start() {
     console.log("App Started");
--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,36 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Home" });
 });
 
+/* GET home page. */
 router.get("/home", function (req, res, next) {
   res.render("index", { title: "Home" });
 });
 
+/* GET about page. */
 router.get("/about", (req, res, next) => {
   res.render("about", { title: "About" });
 });
 
+/* GET projects page. */
 router.get("/projects", (req, res, next) => {
   res.render("projects", { title: "Projects" });
 });
 
+/* GET services page. */
 router.get("/services", (req, res, next) => {
   res.render("services", { title: "Services" });
 });
 
+/* GET contact page. */
 router.get("/contact", (req, res, next) => {
   res.render("contact", { title: "Contact" });
 });
 
-router.post("/home", (req, res, next) => {
-  res.render("home", { title: "Home" });
+/* Post Contact page and redirect to home page */
+router.post("/contact", (req, res, next) => {
+  console.log("entered");
+  console.log(req.body);
+  res.redirect("/home");
 });
 
 module.exports = router;
