83fe50d3a09fa26cc00fce78eb2b1376902bde56
Balachanderrao <palepubalachanderrao25@gmail.com>
2021-02-25 00:23:06 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -2,4 +2,9 @@
 Balachander Rao Palepu, 301170247, 07-02-2021
 */
 
-module.exports = { URI: "mongodb://localhost/business_contacts" };
+module.exports = {
+  URI:
+    "mongodb+srv://balu:SomeSecret@cluster0.n0iz4.mongodb.net/business_contacts?retryWrites=true&w=majority",
+  // "mongodb://localhost/business_contacts",
+  Secret: "SomeSecret",
+};
